e37a9d1789eb3a396483f38ddd4f33886dbfcd77
tloumatlou <69678675+tloumatlou@users.noreply.github.com>
2020-08-18 11:52:08 +0200

    Add files via upload
    Updated the login system, added a getpass function obscure password input, and a file to store user names

--- loginsystem.py
+++ loginsystem.py
@@ -1,3 +1,6 @@
+from getpass import getpass
+
+f=open("names.txt","a")#to store usernames
 A=input("Do you have an account? Y/N: ")
 def login(p):
 	name=input("Enter username: ")
@@ -17,12 +20,13 @@ else:
 		print("Goodbye!")
 	else:
 		newname=input("Enter name: ")
+		f.write(newname)#append name to file
 		while True:
-			newpas=input("Enter password: ")
-			cpass=input("Confirm password:")
+			newpas=getpass("Enter password: ")
+			cpass=getpass("Confirm password:")
 			if newpas != cpass:
 				print("Passwords don't match!")
 			else:
 				print("Created account")
 				break
-	
+f.close()
